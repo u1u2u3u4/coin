ba94f6948c3f6da42f75a656b8bd2262a545707f
yoon <777uku@gmail.com>
2020-04-28 03:58:55 +0900

    Coins table

--- src/routes/Coins/Coins.js
+++ src/routes/Coins/Coins.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import { BASE_URL } from "../../config";
+import Table from "./Section/Table";
 
 function Coins() {
-  return (
-    <div>
-      <div>Coins</div>
-    </div>
-  );
+  const [Coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `${BASE_URL}/coins/markets?vs_currency=krw&order=market_cap_desc&per_page=100&page`
+      )
+      .then((response) => {
+        // console.log(response.data);
+        setCoins(response.data);
+      });
+  }, []);
+
+  return <div>{Coins && <Table coins={Coins} />}</div>;
 }
 
 export default Coins;
